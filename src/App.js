e86a13b5d8b362c85abffd6a175a305d3a4421cc
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-11-10 08:34:52 -0500

    Enable Dark Mode --- It's temporary.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+// Dark Mode Enabled
+import {Switch, Paper} from '@material-ui/core';
+import {ThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 
 import './assets/sass/styles.scss';
 
 import Home from './components/ui/Home';
 
 function App() {
+    const [darkMode, setDarkMode] = useState(false);
+    const theme = createMuiTheme({
+        palette: {
+            type: darkMode ? "dark" : "light",
+        },
+    });
     return (
-        <div className="App">
-            <header className="App-header">
-                <Home />
-            </header>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Paper>
+                <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} style={{paddingLeft: "auto"}} />
+                <div className="App">
+                    <header className="App-header">
+                        <Home />
+                    </header>
+                </div>
+            </Paper>
+        </ThemeProvider>
     );
 }
 
--- src/components/ui/Home.jsx
+++ src/components/ui/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
 
     return (
         <React.Fragment>
-            <div className="container" style={{justifyContent: "center", alignItems: "center", textAlign: "center"}}>
+            <div className="container" style={{height: "100vh", justifyContent: "center", alignItems: "center", textAlign: "center"}}>
                 <Typography variant="h1" style={{fontFamily: "Playfair Display", paddingBottom: "5rem", fontWeight: "800"}}>Welcome</Typography>
                 <img style={{justifyContent: "center", alignItems: "center", paddingBottom: "3rem", width: "50%"}} src={logo} alt={'The Beaver Corporation'} />
                 <h2>Our Page is under heavy construction</h2>
